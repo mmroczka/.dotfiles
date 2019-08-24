14c2ec1398991e5a69ac1efb6de99846c9e43daa
Michael Mroczka <michaelmroczka@gmail.com>
2019-08-24 11:50:49 -0700

    Update

--- hammerspoon/vim_bindings.lua
+++ hammerspoon/vim_bindings.lua
@@ -2,6 +2,7 @@ local message = require('status-message')
 local messageNormal = message.new('normal mode')
 local messageVisual = message.new('visual mode')
 displayStatus = function(text)
+	sendMessageToAllScreens()
 	if text == 'normal' then
 		messageNormal:show()
 	elseif text == 'visual' then
@@ -26,6 +27,25 @@ function mergeArrays(ar1, ar2)
 		table.insert(output, k)
 	end
 	return output
+end
+
+function sendMessageToAllScreens()
+	-- Get the focused window, its window frame dimensions, its screen frame dimensions,
+	-- and the next screen's frame dimensions.
+	print('made it into this function')
+	local focusedWindow = hs.window.focusedWindow()
+	local focusedScreenFrame = focusedWindow:screen():frame()
+	local nextScreenFrame = focusedWindow:screen():next():frame()
+	local windowFrame = focusedWindow:frame()
+
+	-- Calculate the coordinates of the window frame in the next screen and retain aspect ratio
+	windowFrame.x = ((((windowFrame.x - focusedScreenFrame.x) / focusedScreenFrame.w) * nextScreenFrame.w) + nextScreenFrame.x)
+	windowFrame.y = ((((windowFrame.y - focusedScreenFrame.y) / focusedScreenFrame.h) * nextScreenFrame.h) + nextScreenFrame.y)
+	windowFrame.h = ((windowFrame.h / focusedScreenFrame.h) * nextScreenFrame.h)
+	windowFrame.w = ((windowFrame.w / focusedScreenFrame.w) * nextScreenFrame.w)
+
+	-- Set the focused window's new frame dimensions
+	focusedWindow:setFrame(windowFrame)
 end
 
 function mergeTables(t1, t2)
