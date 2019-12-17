fb6be8bc7e2eb2b7d08058f10bea5c3a53a658a9
Michael Mroczka <michaelmroczka@gmail.com>
2019-12-17 13:36:01 -0600

    Update after dropbox sync and fix clear command

--- MacSettingsBackup/AlfredPreferences/Alfred.alfredpreferences/workflows/user.workflow.1416C6E2-89DB-427B-86DF-4BD9BD572FB3/dndenabled.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-val=$(defaults -currentHost read "${HOME}/Library/Preferences/ByHost/com.apple.notificationcenterui" doNotDisturb)
-if [ "$val" = 0 ]
-then
-  echo -n "disabled"
-else
-  echo -n "enabled"
-fi
--- MacSettingsBackup/AlfredPreferences/Alfred.alfredpreferences/workflows/user.workflow.540A200A-35C8-4976-9ABE-DF093AD7A9AB/changeFormat.sh
+++
@@ -1,7 +0,0 @@
-source includes.sh
-
-FORMAT="$1"
-
-echo "xxx $FORMAT" >> /tmp/xxx
-
-storePreference "TIME_FORMAT" "$FORMAT"
