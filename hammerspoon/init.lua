9343a7b89cb3ffc750d38db981fe1b731ae95523
Michael Mroczka <michael.mroczka@gmail.com>
2021-01-19 14:43:46 -0600

    Fix hammerspoon and vim spoon and add to gitignore

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,9 +3,9 @@ local inspect = require "inspect"
 local log = hs.logger.new('init.lua', 'debug')
 
 -- Use Control+` to reload Hammerspoon config
-hs.hotkey.bind({'ctrl'}, '`', nil, function() hs.reload() hs.application.get("Hammerspoon"):selectMenuItem("Reload Config") end)
+-- hs.hotkey.bind({'ctrl'}, '`', nil, function() hs.reload() hs.application.get("Hammerspoon"):selectMenuItem("Reload Config") end)
 
-hs.hotkey.bind({'ctrl'}, '1', nil, function() hs.reload() hs.application.get("Hammerspoon"):selectMenuItem("Console...") hs.application.launchOrFocus("Hammerspoon") end)
+-- hs.hotkey.bind({'ctrl'}, '1', nil, function() hs.reload() hs.application.get("Hammerspoon"):selectMenuItem("Console...") hs.application.launchOrFocus("Hammerspoon") end)
 
 hs.urlevent.bind("restartHammerSpoon", function(eventName, params) hs.reload() hs.application.get("Hammerspoon"):selectMenuItem("Reload Config") end)
 
@@ -14,8 +14,8 @@ flux.restoreScreen()
 hs.loadSpoon("Lunette")
 spoon.Lunette:bindWebHooks()
 
-hs.loadSpoon("Vim")
-local v = spoon.Vim:new()
+hs.loadSpoon("VimSpoon")
+local v = spoon.VimSpoon:new()
 
 v:setDebug(true) -- uncoment this if you want some things printed to the hammerspoon console
 v:start()
